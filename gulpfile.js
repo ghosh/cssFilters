a9a29ebad8b6294f4136319a08dca790618364be
Indrashish <mail@indrashishghosh.com>
2015-11-24 13:51:07 +0530

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,14 @@ var gulp        = require('gulp'),
     scsslint = require('gulp-scss-lint'),
     scssLintStylish = require('gulp-scss-lint-stylish'),
     cache = require('gulp-cached'),
+
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    reactify = require('reactify'),
+
     argv        = require('yargs').argv,
     reload      = browserSync.reload;
 
@@ -69,6 +77,30 @@ gulp.task('styles', function() {
     .pipe(reload({ stream:true }));
 });
 
+
+gulp.task('scripts', function () {
+  var b = browserify({
+    entries: './source/js/inbox.js',
+    debug: true,
+    transform: [reactify]
+  });
+  // b.external('react')
+  // b.external('react-dom')
+
+  return b.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(gulpif(argv.build, uglify()))
+    .on('error', notify.onError({ message: 'Error: <%= error.message %>'}))
+        // .on('error', gutil.log)
+    .pipe(sourcemaps.write(config.scripts.distDirectory))
+    .pipe(rename(config.scripts.distFile))
+    .pipe(gulp.dest(config.scripts.distDirectory))
+    .pipe(reload({ stream:true }));
+});
+
+
 gulp.task('lint:styles', function() {
   return gulp.src('source/scss/**/*.scss')
     .pipe(cache('scsslint'))
@@ -80,7 +112,7 @@ gulp.task('lint:styles', function() {
 
 
 gulp.task('lint', ['lint:styles'])
-gulp.task('compile', ['styles'])
+gulp.task('compile', ['styles', 'scripts'])
 
 gulp.task('go', ['compile', 'lint'],function() {
   browserSync({
@@ -93,5 +125,6 @@ gulp.task('go', ['compile', 'lint'],function() {
   // gulp.watch('source/**/*.hbs', ['hbs']);
   // gulp.watch('source/assets/svg/**/*.svg', ['svg']);
   // gulp.watch('source/assets/images/*', ['images']);
+  gulp.watch(config.scripts.srcDirectory + '*', ['scripts']);
   gulp.watch(config.styles.srcDirectory + '**/*.scss', ['styles', 'lint:styles']);
 });
